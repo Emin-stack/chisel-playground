41323c6da0c49c484ad6a067f85b4f43bccb6594
Qiming Chu <cchuqiming@gmail.com>
2024-08-26 23:03:28 +0800

    playground: Reformat examples c code

--- playground/test/src/examples/main.cc
+++ playground/test/src/examples/main.cc
@@ -1,27 +1,27 @@
+#include "Vtb.h"
 #include <verilated.h>
 #include <verilated_vcd_c.h>
-#include "Vtb.h"
 
-int main(int argc, char** argv){
-	Verilated::debug(0);
-  VerilatedContext* contextp = new VerilatedContext;
-  VerilatedVcdC*    tracep   = new VerilatedVcdC;
-  Vtb*     				  duvp     = new Vtb;
+int main(int argc, char **argv) {
+  Verilated::debug(0);
+  VerilatedContext *contextp = new VerilatedContext;
+  VerilatedVcdC *tracep = new VerilatedVcdC;
+  Vtb *duvp = new Vtb;
 
-	contextp->fatalOnError(false);
+  contextp->fatalOnError(false);
   contextp->commandArgs(argc, argv);
   contextp->traceEverOn(true);
-  duvp->trace(tracep,3);
+  duvp->trace(tracep, 3);
   tracep->open("waveform.vcd");
-  while (!contextp->gotFinish()){
+  while (!contextp->gotFinish()) {
     duvp->eval();
     tracep->dump(contextp->time());
     contextp->timeInc(1);
-		if (!duvp->eventsPending())
+    if (!duvp->eventsPending())
       break;
-		contextp->time(duvp->nextTimeSlot());
+    contextp->time(duvp->nextTimeSlot());
   }
-	if (!contextp->gotFinish()) {
+  if (!contextp->gotFinish()) {
     VL_DEBUG_IF(VL_PRINTF("+ Exiting without $finish; no events left\n"););
   }
   tracep->close();
